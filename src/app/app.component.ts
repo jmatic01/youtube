0427e0c2306a5ebf38deec92270741e5a6cc04ab
Josip Matić <josip@slicejack.com>
2017-08-11 14:13:24 +0200

    Application update

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component, ViewEncapsulation} from "@angular/core";
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  encapsulation: ViewEncapsulation.None,
+
+})
+export class AppComponent {
+  title = 'Youtube Search';
+}
---
+++ src/app/videos/videos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { VideoListComponent } from './video-list'
+import { VideoDetailComponent } from './video-detail'
+
+@Component({
+	selector: 'dl-videos',
+	templateUrl: 'videos.component.html'
+})
+export class VideosComponent {
+	constructor() {}
+}
